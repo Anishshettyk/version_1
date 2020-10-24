be47b14dde2dab017231472a4f47bc31b6b77ec5
Anish shetty k <anishshettyk@gmail.com>
2020-10-24 17:11:34 +0530

    another file path error resolved for global styles

--- src/styles/index.js
+++ src/styles/index.js
@@ -1,3 +1,3 @@
-export { default as GlobalStyles } from './GlobalStyles';
+export { default as GlobalStyles } from './globalStyles';
 export { default as theme } from './theme';
 export { default as mixin } from './mixin';
