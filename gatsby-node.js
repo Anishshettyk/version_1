b08af7133149607f6b40915a000f57109e3eda7f
Anish shetty k <anishshettyk@gmail.com>
2020-11-29 10:43:54 +0530

    error resolved with webpack for bad module

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,5 +1,20 @@
 const path = require('path');
 
+exports.onCreateWebpackConfig = ({ stage, loaders, actions }) => {
+  if (stage === 'build-html') {
+    actions.setWebpackConfig({
+      module: {
+        rules: [
+          {
+            test: /scrollreveal/,
+            use: loaders.null(),
+          },
+        ],
+      },
+    });
+  }
+};
+
 exports.onCreateWebpackConfig = ({ actions }) => {
   actions.setWebpackConfig({
     resolve: {
