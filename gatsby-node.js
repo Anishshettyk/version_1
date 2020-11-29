0287b1c4d0ccab3dedb39f9f07ecb1878b187267
Anish shetty k <anishshettyk@gmail.com>
2020-11-29 11:16:19 +0530

    simple error resolved

--- gatsby-node.js
+++ gatsby-node.js
@@ -13,19 +13,19 @@ exports.onCreateWebpackConfig = ({ stage, loaders, actions }) => {
       },
     });
   }
-};
 
-exports.onCreateWebpackConfig = ({ actions }) => {
-  actions.setWebpackConfig({
-    resolve: {
-      alias: {
-        '@components': path.resolve(__dirname, 'src/components'),
-        '@styles': path.resolve(__dirname, 'src/styles'),
-        '@fonts': path.resolve(__dirname, 'src/fonts'),
-        '@hooks': path.resolve(__dirname, 'src/hooks/'),
-        '@blobs': path.resolve(__dirname, 'src/stableData/blobs'),
-        '@utils': path.resolve(__dirname, 'src/utils/'),
+  exports.onCreateWebpackConfig = ({ actions }) => {
+    actions.setWebpackConfig({
+      resolve: {
+        alias: {
+          '@components': path.resolve(__dirname, 'src/components'),
+          '@styles': path.resolve(__dirname, 'src/styles'),
+          '@fonts': path.resolve(__dirname, 'src/fonts'),
+          '@hooks': path.resolve(__dirname, 'src/hooks/'),
+          '@blobs': path.resolve(__dirname, 'src/stableData/blobs'),
+          '@utils': path.resolve(__dirname, 'src/utils/'),
+        },
       },
-    },
-  });
+    });
+  };
 };
